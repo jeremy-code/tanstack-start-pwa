9d99e53f261ee633ccb1dbb34387bb6b780d9a96
Jeremy Nguyen <nguyen.jeremyt@gmail.com>
2026-09-14 17:24:54 -0700

    chore: update Serwist config

--- src/sw.ts
+++ src/sw.ts
@@ -1,14 +1,11 @@
 /// <reference lib="webworker" />
 
 import { defaultCache } from "@serwist/vite/worker";
-import type { PrecacheEntry, SerwistGlobalConfig } from "serwist";
-import { Serwist } from "serwist";
+import { Serwist, type PrecacheEntry, type SerwistGlobalConfig } from "serwist";
 
 declare global {
   interface WorkerGlobalScope extends SerwistGlobalConfig {
-    // Change this attribute's name to your `injectionPoint`.
-    // `injectionPoint` is an InjectManifest option.
-    // See https://serwist.pages.dev/docs/build/configuring
+    // https://serwist.pages.dev/docs/build/configuring/injection-point
     __SW_MANIFEST: (PrecacheEntry | string)[] | undefined;
   }
 }
@@ -16,12 +13,16 @@ declare global {
 declare const self: ServiceWorkerGlobalScope;
 
 const serwist = new Serwist({
-  // eslint-disable-next-line no-underscore-dangle
+  // oxlint-disable-next-line no-underscore-dangle -- This is the convention
   precacheEntries: self.__SW_MANIFEST,
+  precacheOptions: {
+    cleanupOutdatedCaches: true,
+  },
   skipWaiting: true,
-  clientsClaim: true,
   navigationPreload: true,
+  clientsClaim: true,
   runtimeCaching: defaultCache,
+  offlineAnalyticsConfig: false,
 });
 
 serwist.addEventListeners();
